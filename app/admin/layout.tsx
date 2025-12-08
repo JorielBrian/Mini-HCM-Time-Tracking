b328c7611eb0b16617f0020949d8d11a8084cff7
Joriel Brian Sudario <jorielsudario@gmail.com>
2025-12-08 20:32:22 +0800

    created admin layout

---
+++ app/admin/layout.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { auth } from '../../firebase/firebase.config';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const router = useRouter();
+  const user = auth.currentUser;
+
+  useEffect(() => {
+    if (!user) {
+      router.push('/login');
+    }
+  }, [user, router]);
+
+  if (!user) {
+    return null;
+  }
+
+  return <>{children}</>;
+}
