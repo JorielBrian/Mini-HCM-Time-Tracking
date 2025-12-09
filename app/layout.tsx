561dcee10aad81aa15ac2a49f69965a275e25acb
Joriel Brian Sudario <jorielsudario@gmail.com>
2025-12-09 22:11:16 +0800

    added a tab icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Mini HCM Time Tracking",
   description: "Employee time tracking system",
+  icons: '/logo.png',
 };
 
 export default function RootLayout({
